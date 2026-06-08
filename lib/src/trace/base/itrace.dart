0a84d0ca1e0e3b316d2ffeb5a6668ed619a15bdb
Oluwaseyi Fatunmole <ofatunmole@itandtsolutions.net>
2026-06-08 10:42:12 +0100

    added license

--- lib/src/trace/base/itrace.dart
+++ lib/src/trace/base/itrace.dart
@@ -7,5 +7,5 @@ abstract class Trace<T, E> {
 
   E mapError({required E Function(E error) e});
 
-  Trace<B, E> bind<B>({required Future<Trace<B, E>> Function(T value) n});
+  Trace<B, E> bind<B>({required Trace<B, E> Function(T value) n});
 }
